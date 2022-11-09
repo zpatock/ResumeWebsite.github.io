af3170e55e867f6b9f3fe908a31e1006700d34ee
Zachary Patock <zpatock001@csbsju.edu>
2022-11-08 21:11:25 -0600

    Added Final Touches

--- src/Pages/mainPage.js
+++ src/Pages/mainPage.js
@@ -1,12 +1,13 @@
 
+import picture from "../components/icons/pic.jpg"
 
 function MainPage() {
   return (
-    <div><br></br>
+    <div>
       <p className="workCard">
-        <h2 style={{textAlign: "center"}}>ZACHARY PATOCK</h2><br/>
+        <h2 style={{textAlign: "center"}}>ZACHARY PATOCK</h2>
+        <div style={{textAlign: "center", scale: "70%"}}><img src={picture} alt="Picture" ></img></div>
         <h4 style={{textAlign: "center"}}>Contact Information:</h4>
-        <br/>
         <div style={{textAlign: "center"}}>New London, MN-56273 
           | 320-212-6594 | zachary.patock@gmail.com | https://github.com/zpatock<br/><br/></div>
   </p></div>
